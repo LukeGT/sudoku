1daf804e7a5b2559c25a1faef87966d03a98abb8
Luke Tsekouras <luke.tsekouras@gmail.com>
2021-09-23 00:49:36 +1000

    Explore coordinate possibilities, performance improvements

--- index.ts
+++ index.ts
@@ -42,6 +42,7 @@ class Puzzle {
 
   $explore_any = false;
   $explore_any_chase_one = true;
+  $explore_coordinates = true;
 
   solve({
     solutions = [],
@@ -58,12 +59,17 @@ class Puzzle {
   } = {}) {
     if (solutions.length >= solution_num) return solutions;
 
+    if (Math.random() < 1 / 100000) this.print();
+
     let best_number = new Best<{ index: number; possibilities: number[] }>(random && this.$explore_any);
     let best_shading = new Best<{ index: number; possibilities: boolean[] }>(random && this.$explore_any);
 
     for (let index = 0; index < this.numbers.length; ++index) {
       if (numbers && this.numbers[index] === null) {
-        const possibilities = this.number_possibilities(index);
+        const possibilities = this.number_possibilities(
+          index,
+          (best_number.value?.possibilities.length ?? Infinity) + 1,
+        );
         const score = this.$explore_any
           ? Math.min(this.$explore_any_chase_one ? 2 : 1, possibilities.length)
           : possibilities.length;
@@ -71,7 +77,10 @@ class Puzzle {
       }
 
       if (shadings && this.shadings[index] === null) {
-        const possibilities = this.shading_possibilities(index);
+        const possibilities = this.shading_possibilities(
+          index,
+          (best_shading.value?.possibilities.length ?? Infinity) + 1,
+        );
         const score = this.$explore_any
           ? Math.min(this.$explore_any_chase_one ? 2 : 1, possibilities.length)
           : (possibilities.length - 1) * this.size + 1;
@@ -87,15 +96,48 @@ class Puzzle {
       return solutions;
     }
 
-    const min_score = Math.min(best_number.score, best_shading.score);
+    const min_score_so_far = Math.min(best_number.score, best_shading.score);
+    let best_coordinate = new Best<{ number: number; possibilities: number[] }>(random && this.$explore_any);
+
+    if (numbers && this.$explore_coordinates && min_score_so_far > 1) {
+      (() => {
+        for (const coord_type of ['row', 'col', 'box']) {
+          for (let coord_index = 0; coord_index < this.size; ++coord_index) {
+            for (let number = 1; number <= this.size; ++number) {
+              const possibilities = this.coordinate_possibilities(
+                { [coord_type]: coord_index },
+                number,
+                (best_coordinate.value?.possibilities.length ?? Infinity) + 1,
+              );
+              if (possibilities.length === 1 && this.numbers[possibilities[0]] === number) continue;
+              const score = this.$explore_any
+                ? Math.min(this.$explore_any_chase_one ? 2 : 1, possibilities.length)
+                : possibilities.length;
+
+              best_coordinate.consider({ number, possibilities }, score);
+              if (best_coordinate.score < min_score_so_far) return;
+            }
+          }
+        }
+      })();
+    }
+
+    const min_score = Math.min(best_number.score, best_shading.score, best_coordinate.score);
 
     if (best_number.score === min_score && best_number.value !== null) {
-      for (const number of random
-        ? _.shuffle(best_number.value.possibilities)
-        : best_number.value.possibilities ?? []) {
+      for (const number of random ? _.shuffle(best_number.value.possibilities) : best_number.value.possibilities) {
         this.numbers[best_number.value.index] = number;
         this.solve({ solutions, solution_num, random });
         this.numbers[best_number.value.index] = null;
+        if (solutions.length >= solution_num) return solutions;
+      }
+    } else if (best_coordinate.score === min_score && best_coordinate.value !== null) {
+      for (const index of random
+        ? _.shuffle(best_coordinate.value.possibilities)
+        : best_coordinate.value.possibilities) {
+        this.numbers[index] = best_coordinate.value.number;
+        this.solve({ solutions, solution_num, random });
+        this.numbers[index] = null;
         if (solutions.length >= solution_num) return solutions;
       }
     } else if (best_shading.score === min_score && best_shading.value !== null) {
@@ -117,46 +159,50 @@ class Puzzle {
   }: { max_reduce?: number; numbers?: boolean; shadings?: boolean } = {}) {
     console.log();
 
+    const required_numbers = Array.from(this.numbers, () => false);
+    const required_shadings = Array.from(this.shadings, () => false);
+
     for (let reductions = 0; reductions < max_reduce; ++reductions) {
       process.stdout.write(`\rReductions: ${reductions} + 0 `);
-      const best_number = new Best<{ index: number; possibilities: number[] }>(true);
-      const best_shading = new Best<{ index: number; possibilities: boolean[] }>(true);
+      const options: { type: 'number' | 'shading'; index: number; score: number }[] = [];
 
       for (let index = 0; index < this.size * this.size; ++index) {
-        if (numbers && this.numbers[index] !== null) {
-          process.stdout.write(`\rReductions: ${reductions} + ${index}`);
-          const number_possibilities = this.number_possibilities(
-            index,
-            (best_number.value?.possibilities.length ?? Infinity) + 1,
-          );
-          // TODO: Refactor this so that it doesn't call is_number_needed once for everything equal score
-          if (number_possibilities.length <= best_number.score && !this.is_number_needed(index)) {
-            best_number.consider({ index, possibilities: number_possibilities }, number_possibilities.length);
-          }
+        if (numbers && this.numbers[index] !== null && !required_numbers[index]) {
+          const number_possibilities = this.number_possibilities(index);
+          const score = number_possibilities.length;
+          options.push({ type: 'number', index, score });
         }
-        if (shadings && this.shadings[index] !== null) {
-          process.stdout.write(`\rReductions: ${reductions} + ${index}`);
-          const shading_possibilities = this.shading_possibilities(
-            index,
-            (best_shading.value?.possibilities.length ?? Infinity) + 1,
-          );
+        if (shadings && this.shadings[index] !== null && !required_shadings[index]) {
+          const shading_possibilities = this.shading_possibilities(index);
           const score = (shading_possibilities.length - 1) * this.size + 1;
-          if (score <= best_shading.score && !this.is_shading_needed(index)) {
-            best_shading.consider({ index, possibilities: shading_possibilities }, score);
-          }
+          options.push({ type: 'shading', index, score });
         }
       }
 
-      if (best_number.value === null && best_shading.value === null) {
-        break;
-      }
-
-      const min_score = Math.min(best_number.score, best_shading.score);
-      if (best_number.score === min_score && best_number.value) {
-        this.numbers[best_number.value.index] = null;
-      } else if (best_shading.score === min_score && best_shading.value) {
-        this.shadings[best_shading.value.index] = null;
-      }
+      options.sort((a, b) => a.score - b.score);
+      const reduced = (() => {
+        let step = 0;
+        for (const option of options) {
+          process.stdout.write(`\rReductions: ${reductions} + ${++step}`);
+          if (option.type === 'number') {
+            if (this.is_number_needed(option.index)) {
+              required_numbers[option.index] = true;
+            } else {
+              this.numbers[option.index] = null;
+              return true;
+            }
+          } else if (option.type === 'shading') {
+            if (this.is_shading_needed(option.index)) {
+              required_shadings[option.index] = true;
+            } else {
+              this.shadings[option.index] = null;
+              return true;
+            }
+          }
+        }
+        return false;
+      })();
+      if (!reduced) break;
     }
 
     console.log();
@@ -170,16 +216,16 @@ class Puzzle {
     }
     const original_number = this.numbers[index];
     const solutions: Puzzle[] = [];
-    for (let shading = 1; shading <= this.size; ++shading) {
-      if (shading === original_number) continue;
-      this.numbers[index] = shading;
+    for (let number = 1; number <= this.size; ++number) {
+      if (number === original_number) continue;
+      this.numbers[index] = number;
       this.solve({ solutions, solution_num: 1 });
     }
     this.numbers[index] = original_number;
     return solutions.length > 0;
   }
 
-  // TODO: have numbers switche
+  // TODO: have numbers switch
   is_shading_needed(index: number) {
     const possibilities = this.shading_possibilities(index, 2);
     if (possibilities.length === 1) {
@@ -217,7 +263,7 @@ class Puzzle {
       for (const neighbour_index of this.neighbours(index)) {
         const neighbour_number = this.numbers[neighbour_index];
         const neighbour_shading = this.shadings[neighbour_index];
-        // Shadings must be different for rules to apply
+        // Neighbour must have number and shadings must be different for rules to apply
         if (neighbour_number === null || neighbour_shading === null || neighbour_shading === shading) continue;
         // Numbers in shaded cells are greater than their unshaded neighbours, and vice versa
         if (number > neighbour_number !== shading) {
@@ -303,6 +349,35 @@ class Puzzle {
     return possibilities;
   }
 
+  coordinate_possibilities(
+    coords: { row?: number; col?: number; box?: number },
+    number: number,
+    possibility_num = Infinity,
+  ) {
+    const possibilities: number[] = [];
+
+    for (let index = 0; index < this.numbers.length; ++index) {
+      const original_number = this.numbers[index];
+      if (original_number !== null && original_number !== number) continue;
+
+      const this_coords = this.get_coordinates(index);
+      if (this_coords.row !== coords.row && this_coords.col !== coords.col && this_coords.box !== coords.box) continue;
+
+      // Assume that the grid is valid, and that this number has already been placed
+      if (original_number === number) return [index];
+
+      this.numbers[index] = number;
+      if (this.check(index)) {
+        possibilities.push(index);
+      }
+      this.numbers[index] = original_number;
+
+      if (possibilities.length >= possibility_num) break;
+    }
+
+    return possibilities;
+  }
+
   get_coordinates(index: number) {
     const row = Math.floor(index / this.size);
     const col = index % this.size;
@@ -381,15 +456,26 @@ function time<T>(task: () => T, runs = 1) {
   return result;
 }
 
-{
+const t = true;
+const f = false;
+const n = null;
+
+const solve_basic = () => {
   const puzzle = new Puzzle(
     9,
     3,
     3,
+    // prettier-ignore
     [
-      0, 0, 0, 0, 6, 0, 0, 2, 0, 0, 0, 7, 0, 2, 9, 0, 0, 0, 4, 0, 0, 0, 0, 1, 7, 3, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 5,
-      0, 8, 7, 0, 3, 0, 0, 0, 0, 9, 0, 0, 6, 0, 0, 0, 0, 0, 4, 7, 0, 0, 0, 0, 0, 9, 0, 5, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0,
-      5, 8, 0, 0, 1,
+      0, 0, 0, 0, 6, 0, 0, 2, 0,
+      0, 0, 7, 0, 2, 9, 0, 0, 0,
+      4, 0, 0, 0, 0, 1, 7, 3, 0,
+      0, 0, 0, 0, 0, 0, 0, 8, 0,
+      0, 5, 0, 8, 7, 0, 3, 0, 0,
+      0, 0, 9, 0, 0, 6, 0, 0, 0,
+      0, 0, 4, 7, 0, 0, 0, 0, 0,
+      9, 0, 5, 0, 0, 0, 0, 0, 0,
+      6, 0, 0, 0, 5, 8, 0, 0, 1,
     ],
   );
 
@@ -397,16 +483,19 @@ function time<T>(task: () => T, runs = 1) {
   puzzle.$explore_any = true;
   puzzle.$explore_any_chase_one = true;
   puzzle.$basic_shade_explore = true;
+  puzzle.$explore_coordinates = false;
   time(() => puzzle.solve({ solution_num: 2, random: true }), 10);
 
-  console.log('explore min basic shading');
+  console.log('explore min basic');
   puzzle.$explore_any = false;
   puzzle.$basic_shade_explore = true;
+  puzzle.$explore_coordinates = false;
   time(() => puzzle.solve({ solution_num: 2, random: true }), 10);
 
-  console.log('explore min');
+  console.log('explore min complex');
   puzzle.$explore_any = false;
-  puzzle.$basic_shade_explore = false;
+  puzzle.$basic_shade_explore = true;
+  puzzle.$explore_coordinates = true;
   const solutions = time(() => puzzle.solve({ solution_num: 2, random: true, shadings: false }), 10);
 
   console.log('solutions:', solutions.length);
@@ -414,17 +503,67 @@ function time<T>(task: () => T, runs = 1) {
   console.log('not-improved:', puzzle.$times_identical_shading);
   puzzle.print();
   solutions[0].print();
-}
+};
 
-{
-  const new_solve = new Puzzle(6, 3, 2).solve({ solution_num: 1, random: true });
+const solve_fortress = () => {
+  const puzzle = new Puzzle(
+    9,
+    3,
+    3,
+    // prettier-ignore
+    [
+      0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 1, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 3, 0, 0,
+      0, 3, 0, 0, 0, 0, 0, 0, 7,
+      0, 0, 4, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 7, 0, 5, 6, 0, 8,
+      0, 0, 0, 0, 0, 0, 0, 6, 0,
+      0, 5, 0, 0, 1, 9, 0, 0, 0,
+      0, 0, 2, 0, 0, 7, 0, 5, 0,
+    ],
+    // prettier-ignore
+    [
+      t, t, f, t, f, n, f, t, n,
+      f, t, n, f, n, n, t, n, n,
+      f, t, t, f, t, f, n, t, n,
+      n, t, n, f, n, f, f, f, f,
+      t, n, n, n, t, t, n, f, f,
+      f, n, t, n, n, n, n, t, n,
+      n, n, t, t, f, t, t, n, n,
+      n, n, f, n, n, n, f, t, t,
+      f, n, n, n, n, n, f, n, f,
+    ],
+  );
 
+  console.log('solving big fortress (complex, early stop)');
+  puzzle.$explore_coordinates = true;
+  time(() => puzzle.solve({ solution_num: 1 }));
+  console.log('improved:', puzzle.$times_improved_shading);
+  console.log('not-improved:', puzzle.$times_identical_shading);
+  console.log('solving big fortress (basic, early stop)');
+  puzzle.$explore_coordinates = false;
+  time(() => puzzle.solve({ solution_num: 1 }));
+  console.log('solving big fortress (complex, exhaustive)');
+  puzzle.$explore_coordinates = true;
+  time(() => puzzle.solve({ solution_num: 2 }));
+  console.log('improved:', puzzle.$times_improved_shading);
+  console.log('not-improved:', puzzle.$times_identical_shading);
+  console.log('solving big fortress (basic, exhaustive)');
+  puzzle.$explore_coordinates = false;
+  const solution = time(() => puzzle.solve({ solution_num: 2 }));
+
+  solution[0].print();
+};
+
+const generate = () => {
   console.log('generating new puzzle');
+  const new_solve = new Puzzle(4, 2, 2, []).solve({ solution_num: 1, random: true });
+
   new_solve[0].print();
 
-  time(() => {
+  return time(() => {
     const new_puzzle = new_solve[0].clone();
-    new_puzzle.$basic_shade_explore = true;
     new_puzzle.reduce();
     console.log('improved:', new_puzzle.$times_improved_shading);
     console.log('not-improved:', new_puzzle.$times_identical_shading);
@@ -437,5 +576,12 @@ function time<T>(task: () => T, runs = 1) {
       solutions[1].print();
       throw new Error('NON UNIQUE');
     }
+    return new_puzzle;
   });
+};
+
+let puzzle: Puzzle | null = null;
+while (puzzle === null || puzzle?.numbers.some((number) => number !== null)) {
+  puzzle = generate();
 }
+puzzle?.print();
